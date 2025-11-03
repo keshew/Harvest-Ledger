1317d2341cfe8a1bca0851d381fcda345622f4b0
keshew <artyom.korotckov2017@yandex.ru>
2025-11-03 18:57:05 +0500

    Initial Commit

---
+++ Harvest Ledger/Harvest_LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Harvest_LedgerApp.swift
+//  Harvest Ledger
+//
+//  Created by Артём Коротков on 03.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Harvest_LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
